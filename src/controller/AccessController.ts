1fcbb6bc7afb4342e37111dd46c799b7e435a58d
Joao Pedro <developerjotape@gmail.com>
2025-03-10 11:43:00 -0300

    ✨ feat: verbos create e get implementados

---
+++ src/controller/AccessController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+import { prisma } from "../database/prisma";
+
+export const createAccess = async (req: Request, res: Response) => {
+  try {
+    const { name } = req.body;
+
+    const access = await prisma.access.create({
+      data: {
+        name,
+      },
+    });
+
+    return res.status(200).json(access);
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+};
+
+export const getAccesses = async (req: Request, res: Response) => {
+  try {
+    const accesses = await prisma.access.findMany();
+    return res.status(200).json(accesses);
+  } catch (error) {
+    return res.status(200).json(error);
+  }
+};
