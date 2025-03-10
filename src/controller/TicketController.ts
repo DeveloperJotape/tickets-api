bd4fa9db8ee69bc2295e61e7ab8b16788571ab75
Joao Pedro <developerjotape@gmail.com>
2025-03-10 14:10:52 -0300

    ✨ feat: métodos createTicket e getAllTickets adicionados

---
+++ src/controller/TicketController.ts
@@ -0,0 +1,83 @@
+import { Request, Response } from "express";
+import { prisma } from "../database/prisma";
+
+export const createTicket = async (req: Request, res: Response) => {
+  try {
+    const { title, description, status, priority, assignedToId } = req.body;
+    const { userId } = req.params;
+
+    // Verifica se o usuário que está criando o ticket existe
+    const createdBy = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!createdBy) {
+      return res.status(404).json({ message: "Usuário inexistente!" });
+    }
+
+    // Verifica se o responsável pelo ticket existe
+    const assignedTo = await prisma.user.findUnique({
+      where: { id: assignedToId },
+    });
+
+    if (!assignedTo) {
+      return res
+        .status(404)
+        .json({ message: "Usuário responsável inexistente!" });
+    }
+
+    const ticket = await prisma.ticket.create({
+      data: {
+        title,
+        description,
+        status,
+        priority,
+        CreatedBy: {
+          connect: {
+            id: userId,
+          },
+        },
+        AssignedTo: {
+          connect: {
+            id: assignedToId,
+          },
+        },
+      },
+    });
+
+    return res.status(201).json(ticket);
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+};
+
+export const getAllTickets = async (req: Request, res: Response) => {
+  try {
+    const tickets = await prisma.ticket.findMany({
+      select: {
+        id: true,
+        title: true,
+        description: true,
+        status: true,
+        priority: true,
+        CreatedBy: {
+          select: {
+            name: true,
+          },
+        },
+        AssignedTo: {
+          select: {
+            name: true,
+          },
+        },
+        created_at: true,
+        updated_at: true,
+      },
+    });
+    return res.status(200).json(tickets);
+  } catch (error) {
+    return res.status(404).json(error);
+  }
+};
