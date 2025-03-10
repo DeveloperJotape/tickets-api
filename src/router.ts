c679d281f7623b4ca86ca79015bfaa67584a96a4
Joao Pedro <developerjotape@gmail.com>
2025-03-10 14:11:34 -0300

    ✨ feat: verbos post e get referentes a Ticket adicionados

--- src/router.ts
+++ src/router.ts
@@ -1,14 +1,24 @@
 import { Router } from "express";
 
-import { createUser, deleteManyUser } from "./controller/UserController";
+import {
+  createUser,
+  deleteManyUser,
+  getAllUsers,
+} from "./controller/UserController";
 import { createAccess, getAccesses } from "./controller/AccessController";
+import { createTicket, getAllTickets } from "./controller/TicketController";
 
 export const router = Router();
 
 // Users
 router.post("/user", createUser);
+router.get("/user", getAllUsers);
 router.delete("/user/delete-users", deleteManyUser);
 
 // Accesses
 router.post("/access", createAccess);
 router.get("/access", getAccesses);
+
+// Ticket
+router.post("/ticket/:userId", createTicket);
+router.get("/ticket", getAllTickets);
