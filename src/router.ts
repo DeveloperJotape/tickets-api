8d9d1bb35e60c4856e947fc3f1fd3a85e03a4685
Joao Pedro <developerjotape@gmail.com>
2025-03-11 16:32:08 -0300

    ✨ feat: rotas com autenticação

--- src/router.ts
+++ src/router.ts
@@ -4,6 +4,8 @@ import {
   createUser,
   deleteManyUser,
   getAllUsers,
+  getUniqueUser,
+  
 } from "./controller/UserController";
 import {
   createAccess,
@@ -14,25 +16,26 @@ import {
   createTicket,
   deleteManyTickets,
   getAllTickets,
-  getTicketsByUser,
+  getTicketsByUserId,
 } from "./controller/TicketController";
 import { signIn } from "./controller/SessionController";
 import { authMiddleware } from "./middleware/AuthMiddleware";
 
 export const router = Router();
 
-// Users
-router.post("/user", authMiddleware(["Administrador", "Gerente"]), createUser);
-router.get("/user", authMiddleware(["Administrador", "Gerente"]), getAllUsers);
+// Usuários
+router.post("/user", authMiddleware(["Administrador", "Gerente"]),createUser);
+router.get("/user", authMiddleware(["Administrador"]), getAllUsers);
+router.get("/user/get-unique-user", authMiddleware(["Administrador", "Gerente", "Colaborador"]), getUniqueUser);
 router.delete(
   "/user/delete-users",
   authMiddleware(["Administrador"]),
   deleteManyUser
 );
 
-// Accesses
+// Níveis de acesso
 router.post("/access", authMiddleware(["Administrador"]), createAccess);
-router.get("/access", authMiddleware(["Administrador"]), getAccesses);
+router.get("/access", authMiddleware(["Administrador", "Gerente", "Colaborador"]), getAccesses);
 router.delete(
   "/access/delete-accesses",
   authMiddleware(["Administrador"]),
@@ -40,10 +43,10 @@ router.delete(
 );
 
 // Ticket
-router.post("/ticket/:userId", createTicket);
-router.get("/ticket", getAllTickets);
-router.get("/ticket/:userId", getTicketsByUser);
-router.delete("/ticket/delete-tickets", deleteManyTickets);
+router.post("/ticket", authMiddleware(["Administrador", "Gerente", "Colaborador"]), createTicket);
+router.get("/ticket", authMiddleware(["Administrador"]), getAllTickets);
+router.get("/ticket/:userId", authMiddleware(["Administrador", "Gerente"]), getTicketsByUserId);
+router.delete("/ticket/delete-tickets", authMiddleware(["Administrador"]),deleteManyTickets);
 
 // Autenticação
 router.post("/sign-in", signIn);
